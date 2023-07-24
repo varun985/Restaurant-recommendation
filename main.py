d103acd7b566c188f72e2759385870eead69cc47
Varun Konkimalla <73933440+varun985@users.noreply.github.com>
2023-07-25 02:45:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import clips
 import pandas as pd
-import tkinter as tk
-from tkinter import messagebox
 
 # Load the CLIPS environment
 env = clips.Environment()
@@ -13,77 +11,39 @@ env.load("rules.clp")
 df = pd.read_csv('restaurantDATA.csv')
 
 # Extract the relevant columns from the DataFrame
-cities = df['CITY']
+locations = df['LOCATION']
 categories = df['CATEGORY']
 cuisines = df['CUISINE']
 prices = df['PRICE']
 ratings = df['RATING']
 restaurantnames = df['RESTAURANT NAME']
 
-# Function to handle the recommendation logic
-def recommend_restaurant():
-    # Get the user's preferences from the input fields
-    city = city_entry.get()
-    category = category_entry.get()
-    cuisine = cuisine_entry.get()
-    price = price_entry.get()
-    rating = rating_entry.get()
+# Iterate over the DataFrame rows
+# for i in range(len(df)):
+#     # Create a Restaurant fact in CLIPS for each row in the DataFrame
+#     env.assert_string(f'(Restaurant (city "{cities[i]}") (category "{categories[i]}") (cuisine "{cuisines[i]}") (price "{prices[i]}") (rating "{ratings[i]}"))')
 
-    # Create a UserRecommendation fact in CLIPS based on user inputs
-    env.assert_string(f'(Restaurant (city {city}) (category {category}) (cuisine {cuisine}) (price {price}) (rating {rating}))')
+# Define the user's preferences
+location = input("Enter the location: ")
+category = input("Enter the category (Veg/Non-Veg): ")
+cuisine = input("Enter the cuisine (Indian/Chinese/Italian): ")
+price = input("Enter the price range (Under150/150-300/Above300): ")
+rating = input("Enter the minimum rating (1-3): ")
+# location = "Hyderabad"
+# category = "Veg"
+# cuisine = "Italian"
+# price = "Under150"
+# rating = "2"
 
-    # Run the CLIPS engine
-    num = env.run()
+# Create a UserRecommendation fact in CLIPS based on user inputs
+env.assert_string(f'(Restaurant (location {location}) (category {category}) (cuisine {cuisine}) (price {price}) (rating {rating}))')
 
-    # Retrieve the recommendation from CLIPS
-    recommendation = None
-    for fact in env.facts():
-        if 'recommended' in str(fact):
-            recommendation = str(fact)
-            break
+# Run the CLIPS engine
+# DOES NOT DO ANYTHING...
+num = env.run()
+for fact in env.facts():
+    if(fact):
+        print(fact)
 
-    # Reset the CLIPS environment
-    env.reset()
-
-    # Show the recommendation in a message box
-    if recommendation:
-        messagebox.showinfo("Recommendation", recommendation)
-    else:
-        messagebox.showinfo("Recommendation", "No restaurant found matching your criteria.")
-
-# Create the main window
-window = tk.Tk()
-window.title("Restaurant Recommendation System")
-
-# Create labels and input fields
-city_label = tk.Label(window, text="City:")
-city_label.pack()
-city_entry = tk.Entry(window)
-city_entry.pack()
-
-category_label = tk.Label(window, text="Category (Veg/Non-Veg):")
-category_label.pack()
-category_entry = tk.Entry(window)
-category_entry.pack()
-
-cuisine_label = tk.Label(window, text="Cuisine (Indian/Chinese/Italian):")
-cuisine_label.pack()
-cuisine_entry = tk.Entry(window)
-cuisine_entry.pack()
-
-price_label = tk.Label(window, text="Price Range (Under150/150-300/Above300):")
-price_label.pack()
-price_entry = tk.Entry(window)
-price_entry.pack()
-
-rating_label = tk.Label(window, text="Minimum Rating (1-3):")
-rating_label.pack()
-rating_entry = tk.Entry(window)
-rating_entry.pack()
-
-# Create a button to trigger the recommendation
-button = tk.Button(window, text="Recommend Restaurant", command=recommend_restaurant)
-button.pack()
-
-# Run the main event loop
-window.mainloop()
+# Reset the CLIPS environment
+env.reset()
